0237ef203784293e2612cad4dd412795e89cdd93
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-07-08 17:21:54 +0800

    resolve number too big question and configure Jsonbig

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import jsonBig from 'json-bigint'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
+
+request.defaults.transformResponse = [function (data) {
+  try {
+    return jsonBig.parse(data)
+  } catch (err) {
+    return err
+  }
+}]
 
 export default request
