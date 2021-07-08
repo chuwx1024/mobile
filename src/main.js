d08a98bbc8de3cb0270e75176a0abacc9538bcdb
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-07-08 14:41:58 +0800

    rem configer

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { Button } from 'vant'
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
 Vue.use(Button)
