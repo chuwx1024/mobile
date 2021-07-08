87e2f4a61381da1fbfcb7ba7efeb2fda6d889c8f
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-07-08 14:49:00 +0800

    package vant module

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Button } from 'vant'
 import 'amfe-flexible'
+import './utils/register-vant'
 
 Vue.config.productionTip = false
-Vue.use(Button)
 
 new Vue({
   router,
