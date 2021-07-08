1890fd94da19bee874d6242d6a575b3892f05765
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-07-08 09:10:50 +0800

    import vant

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- <router-view/> -->
+    <van-button plain type="primary">朴素按钮</van-button>
+<van-button plain type="info">朴素按钮</van-button>
   </div>
 </template>
 
